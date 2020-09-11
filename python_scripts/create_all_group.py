3800b281cebf6d664d4ba454ced5b71f4f7f631f
jinkang23 <jinkang23@users.noreply.github.com>
2020-09-11 14:51:22 -0500

    updated

---
+++ python_scripts/create_all_group.py
@@ -0,0 +1,8 @@
+domain = data.get('domain', '')
+group = data.get('group', '')
+
+if not isinstance(domain, str) or not isinstance(group, str) or not domain or not group:
+    logger.warning("bad domain or group! Not executing.")
+else:
+    service_data = {"object_id": group, "entities": hass.states.entity_ids(domain)}
+    hass.services.call("group", "set", service_data, False)
